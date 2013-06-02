8b4919e5f00c2c52c0c401ba941b791466b14993
Seven Du <dujinfang@gmail.com>
2013-06-03 00:34:17 +0800

    update test sig

--- test/weixin.sh
+++ test/weixin.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 HOST="localhost:8078"
-HOST="weixin.freeswitch.org.cn:8078"
 Token="FreeSWITCH-ROCKS"
 ROOT="/weixin"
 
 case "$1" in
 	get)
-		URL="?signature=5daf5ae19cc111d0cd6d1e992913b409a65a161b&timestamp=1370185630&nonce=1370371748&echostr=echostr"
+		URL="?signature=233fe5c2168d23d3c63074d7adf2e2ab0f20f6db&timestamp=1370185630&nonce=1370371748&echostr=echostr"
 		;;
 
 	*)
